4aa1cea49c651eb90baed5436c95a39ed2c5a18b
Sarah Lopez <Sarahanne.pez@gmail>
2019-06-20 23:35:19 -0700

    First round of updates to HTML, CSS, & JS.

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+
+
+
+let questions = [
+      { answer: 'b',
+        question: 'What were Boston terriers originally bred for?',
+        options: ['Hunting', 'Fighting', 'Snoring', 'Herding']
+      },
+      { answer: 'c',
+        question: 'Which group do Boston terriers belong to?',
+        options: ['Iphone', 'Apple Watch', 'Ipad']
+      },
+      { answer: 'a',
+        'question': 'Boston terriers are the state dog for which state??',
+        'options': ['Iphone', 'Apple Watch', 'Ipad']
+      },
+      { answer: 'true',
+        'question': 'True or false? Boston terriers were the first official breed created in the US.',
+        'options': ['True', 'False']
+      },
+      { answer: 'super true',
+        'question': 'True or false? Boston terriers are the best dogs.',
+        'options': ['True', 'False', 'Super True']
+      },
+    ]
+
+
+
+
+
