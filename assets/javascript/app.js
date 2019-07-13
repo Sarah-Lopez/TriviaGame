c49e1bc5255149df6cbae4cb450d1407a0793a2b
Sarah Lopez <Sarahanne.pez@gmail>
2019-07-13 14:10:40 -0700

    Fixed git pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,16 @@
+$('.start').on('click', function(){
+    $('#start').remove();
+        for (var i=0; i<questions.length; i++){
+            $('#subwrapper').append('<h2>' + questions[i].questions + '</h2');
+
+}
 
 
+});
+
+//create start button
+//make a timer
+//display score
 
 let questions = [
       { answer: 'b',
@@ -8,11 +19,11 @@ let questions = [
       },
       { answer: 'c',
         question: 'Which group do Boston terriers belong to?',
-        options: ['Iphone', 'Apple Watch', 'Ipad']
+        options: ['Toy', 'Sporting', 'Nonsporting']
       },
       { answer: 'a',
         'question': 'Boston terriers are the state dog for which state??',
-        'options': ['Iphone', 'Apple Watch', 'Ipad']
+        'options': ['Massachusetts', 'India', 'Hawaii']
       },
       { answer: 'true',
         'question': 'True or false? Boston terriers were the first official breed created in the US.',
@@ -23,8 +34,38 @@ let questions = [
         'options': ['True', 'False', 'Super True']
       },
     ]
+    
+    for (let y = 0; y < questions.length; y++){
+        let options = questions[y].options;
+        for (let i = 0; i < options.length;i++){
+            let radioInput = $('<input>').text(options[i]);
 
+            radioInput.attr('type', 'radio');
+            radioInput.attr('value', options[i]);
+            radioInput.attr('name', "questions-"+ y)
+            radioInput.val(options[i]);
+            let label = $('<label>').text(options[i])
 
+            $('#options-' + y).append(radioInput);
+            $('#options-' + y).append(label)
+        }
+    }    
 
+    $('.answer').on('click', function(){
+        console.log($(this).attr('id'));
+    })
 
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function(){
+        game.counter--;
+        $('#counter').html(game.counter);
+        if (game.counter <=0){
+            console.log("Time is uo!");
+            game.done();
+        }
+    }
+};
 
