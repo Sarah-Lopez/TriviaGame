b4c4104b74e0399329bcb4f0cb866b913ee626ea
Sarah Lopez <50177775+Sarah-Lopez@users.noreply.github.com>
2020-02-12 11:29:27 -0700

    updating questions/answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -82,45 +82,30 @@ var countStartNumber = 30;
 
 // Question set
 var questions = [{
-  question: "What was the first full length CGI movie?",
-  answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
-  correctAnswer: "Toy Story",
+  question: "Where does the Boston Terrier breed originate from?",
+  answers: ["Texas", "Massachusetts", "New York", "Washington"],
+  correctAnswer: "Massachusetts",
   image: "assets/images/toystory.gif"
 }, {
-  question: "Which of these is NOT a name of one of the Spice Girls?",
-  answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
-  correctAnswer: "Fred Spice",
+  question: "What year was the breed recognized by the AKC?",
+  answers: ["1933", "2016", "1771", "1893"],
+  correctAnswer: "1893",
   image: "assets/images/spicegirls.gif"
 }, {
-  question: "Which NBA team won the most titles in the 90s?",
-  answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
-  correctAnswer: "Chicago Bulls",
+  question: "What is the Boston Terrier's official nicname?",
+  answers: ["American gentleman", "Pug's cousin", "Tuxedo dog", "Penguin dog"],
+  correctAnswer: "American gentleman",
   image: "assets/images/bulls.gif"
 }, {
-  question: "Which group released the hit song, 'Smells Like Teen Spirit'?",
-  answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
-  correctAnswer: "Nirvana",
+  question: "Which group do Boston Terriers belong to?",
+  answers: ["Toy", "Hound", "Non-sporting", "Herding"],
+  correctAnswer: "Non-sporting",
   image: "assets/images/nirvanabark.gif"
 }, {
-  question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
-  answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
-  correctAnswer: "The Lion King",
+  question: "Which of the following is the most commonly reported biological feature of Boston Terriers?",
+  answers: ["Excellent sight", "Flatulence", "Long tail", "Large size"],
+  correctAnswer: "Flatulence",
   image: "assets/images/lionking.gif"
-}, {
-  question: "Finish this line from the Fresh Prince of Bel-Air theme song: \"I whistled for a cab and when it came near, the license plate said...\"",
-  answers: ["Dice", "Mirror", "Fresh", "Cab"],
-  correctAnswer: "Fresh",
-  image: "assets/images/fresh.gif"
-}, {
-  question: "What was Doug's best friend's name?",
-  answers: ["Skeeter", "Mark", "Zach", "Cody"],
-  correctAnswer: "Skeeter",
-  image: "assets/images/skeeter.gif"
-}, {
-  question: "What was the name of the principal at Bayside High in Saved By The Bell?",
-  answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
-  correctAnswer: "Mr.Belding",
-  image: "assets/images/belding.gif"
 }];
 
 // Variable to hold our setInterval
